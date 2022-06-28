9138ede7254d970ba8a967c9a4f5d6fb20b07f9a
Augustin Bégué <augustin.begue@epita.fr>
2022-06-28 08:55:02 +0200

    init: svelte template

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+</script>
+
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p><a href="https://begue.cc">Augustin BÉGUÉ</a></p>
+</footer>
+
+<style>
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,15 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<div class="flex flex-col justify-evenly items-center">
+	<h1 class="text-3xl font-bold underline">Hello world!</h1>
+</div>
