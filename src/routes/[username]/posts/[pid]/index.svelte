ab65db621ef53cba0fc80783b9cb11c7671aa9c8
Augustin Bégué <augustin.begue@epita.fr>
2022-08-13 14:33:39 +0200

    feat(posts): edition

--- src/routes/[username]/posts/[pid]/index.svelte
+++ src/routes/[username]/posts/[pid]/index.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import UserDisplay from '$lib/components/users/UserDisplay.svelte';
+	import { currentUser } from '$lib/modules/firebase/client';
 
 	import type { Post, User } from '@prisma/client';
 
 	export let post: Post & {
 		author: User;
 	};
+
+	$: isAuthor = post.author.uid === $currentUser?.uid;
 </script>
 
-<div class="flex flex-col md:flex-row gap-4 justify-center">
-	<div class="m-4">
+<div class="flex flex-col md:flex-row gap-4 justify-center p-4">
+	<div>
 		<div class="card bg-base-200 min-w-max">
 			<div class="card-body">
 				<UserDisplay user={post.author} />
@@ -24,12 +27,15 @@
 						<li><span class="kbd">{key}:</span> {post.metadataValues[i]}</li>
 					{/each}
 				</ul>
+				{#if isAuthor}
+					<div class="card-actions justify-start">
+						<a class="btn btn-sm btn-primary" href="{post.pid}/edit">edit</a>
+					</div>
+				{/if}
 			</div>
 		</div>
 	</div>
-	<div class="m-4">
-		{#if post}
-			<img class="max-w-full max-h-[70vh]" src={post.imageUrl} alt={post.title} />
-		{/if}
-	</div>
+	{#if post}
+		<img class="max-w-full max-h-[70vh]" src={post.imageUrl} alt={post.title} />
+	{/if}
 </div>
