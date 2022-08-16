24acc76efb26d07a7fefc632683649939ca13aa6
Augustin Bégué <augustin.begue@epita.fr>
2022-08-17 00:00:37 +0200

    fix(posts/pid): fixed load functions

--- src/routes/posts/[pid]/edit/+page.ts
+++ src/routes/posts/[pid]/edit/+page.ts
@@ -1,16 +1,16 @@
-import type { PostsOutput } from '$lib/types/api';
+import type { PostOutput } from '$lib/types/api';
 import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ data, fetch, parent, params }) => {
 	let res = await fetch(`/api/posts/${params.pid}`);
-	const body = (await res.json()) as PostsOutput;
+	const body = (await res.json()) as PostOutput;
 
 	if (res.status != 200) {
 		throw error(res.status, (body as any).message);
 	}
 
 	return {
-		posts: body.posts
+		post: body.post
 	};
 };
