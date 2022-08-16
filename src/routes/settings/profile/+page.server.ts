497dd501d11e0d6ad1bab7a369889af0cc41a24d
Augustin Bégué <augustin.begue@epita.fr>
2022-08-16 23:35:06 +0200

    chore: migration to new sveltekit routing system

--- src/routes/settings/profile/+page.server.ts
+++ src/routes/settings/profile/+page.server.ts
@@ -1,1 +1,11 @@
-export * from '../../api/users/me/+server';
+import type { UserOutput } from '$lib/types/api';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ data, fetch, parent, params }) => {
+	let res = await fetch(`/api/users/me`);
+	const body = (await res.json()) as UserOutput;
+
+	return {
+		user: body.posts
+	};
+};
