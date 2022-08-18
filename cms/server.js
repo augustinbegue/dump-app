7f3970d4b0a6db8733aa61af3951ac7336e1e1cc
Augustin Bégué <augustin.begue@epita.fr>
2022-08-18 11:54:13 +0200

    cms

--- cms/server.js
+++ cms/server.js
@@ -16,10 +16,12 @@ app.get('/healthcheck', (req, res) => {
 app.use(handler);
 
 if (process.env.NODE_ENV === 'production') {
+  let port = process.env.CMS_PORT || 3333;
   https.createServer({
     key: readFileSync(process.env.CERT_PRIVKEY || './certs/privkey.pem'),
     cert: readFileSync(process.env.CERT_FULLCHAIN || './certs/cert.pem'),
-  }, app).listen(process.env.CMS_PORT || 3333);
+  }, app).listen(port);
+  console.log('cms-server: listening on port', port);
 } else {
     app.listen(3000, () => {
         console.log('cms-server: listening on port 3000');
