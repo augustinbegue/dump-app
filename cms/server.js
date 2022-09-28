cb218e750ac8f5958141003b885243e8f3e29011
Augustin Bégué <augustin.begue@epita.fr>
2022-09-28 11:37:22 +0200

    feat(cms): cors

--- cms/server.js
+++ cms/server.js
@@ -1,11 +1,11 @@
 import { handler } from './build/handler.js';
-import { readFileSync } from 'fs';
 import 'dotenv/config';
 import express from 'express';
-import https from 'https';
 import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 app.get('/healthcheck', (req, res) => {
   res.end('ok');
